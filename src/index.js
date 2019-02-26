066d833612a0851a578fd271965ac48b6caee74e
lauragreen <lollymay@gmail.com>
2019-02-26 07:30:43 +0000

    :wrench: Apollo Client setup and config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import registerServiceWorker from "./registerServiceWorker";
 
-import './style.css';
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import "./style.css";
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.TOKEN}`
+  }
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
